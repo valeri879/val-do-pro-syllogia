ddefe18420f17cf0896f643ae765218e40ee2d70
Val Do <valeri.kharitonashvili1@gmail.com>
2023-05-22 19:07:52 +0400

    lecture 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-
-interface User {
-  id: number;
-  name: string;
-}
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,22 +8,6 @@ interface User {
 })
 export class AppComponent implements OnInit {
   
-  /*   public users: User[] = [
-    { id: 3, name: 'Val' },
-    { id: 4, name: 'Gio' },
-    { id: 5, name: 'Beka' },
-    { id: 6, name: 'Nino' },
-    { id: 8, name: 'Beka' },
-  ] */
-
-  /* public email: FormControl = new FormControl('', [Validators.required]);
-  public password: FormControl = new FormControl(''); */
-
-/*   public form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  }); */
-
   public form!: FormGroup;
 
   constructor(
@@ -37,20 +16,44 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this._fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      name: [''],
+      lastName: [''],
+      friendsGroup: this._fb.array([]),
+      parents: this._fb.array([]),
     });
+  }
+
+  addFriend() {
+    this.friends.push(new FormControl('', [Validators.required]));
+  }
+
+  addParents() {
+    const parents = this._fb.group({
+      mother: [''],
+      father: [''],
+    });
+
+    this.parents.push(parents);
+  }
+
+  removeParents(i: number) {
+    this.parents.removeAt(i);
+  }
+
+  removeFriend(i: number) {
+    this.friends.removeAt(i);
+    console.log(i)
+  }
+
+  get friends(): FormArray {
+    return this.form.controls['friendsGroup'] as FormArray;
+  }
+
+  get parents(): FormArray {
+    return this.form.controls['parents'] as FormArray;
   }
 
   public submit(): void {
     console.log(this.form)
-    // console.log(this.email.value, this.password.value);
   }
-
-  /* search(query: string) {
-    console.log(query)
-    const filtered: User[] = this.users.filter(val => query == val.name);
-    console.log(filtered);
-  } */
-
 }
